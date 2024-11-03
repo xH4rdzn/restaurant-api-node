86f7d2a3573ae4f804b2b7643e0af30b1e887610
Igor Coelho <xh4rdzn@gmail.com>
2024-11-03 08:52:26 -0300

    Finalizando o projeto

--- src/controllers/orders-controller.ts
+++ src/controllers/orders-controller.ts
@@ -35,7 +35,57 @@ class OrdersController {
         throw new AppError("Product not found")
       }
 
-      return response.status(201).json(product)
+      await knex<OrderRepository>("orders").insert({
+        table_session_id,
+        product_id,
+        quantity,
+        price: product.price
+      })
+
+      return response.status(201).json()
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async index(request: Request, response: Response, next: NextFunction) {
+    try {
+      const { table_session_id } = request.params
+
+      const order = await knex("orders")
+      .select("orders.id", 
+        "orders.table_session_id",
+        "orders.product_id",
+        "products.name",
+        "orders.price",
+        "orders.quantity",
+        knex.raw("(orders.price * orders.quantity) AS total"),
+        "orders.created_at",
+        "orders.updated_at"
+      )
+      .join("products", "products.id", "orders.product_id")
+      .where({ table_session_id })
+      .orderBy("orders.created_at", "desc")
+
+      return response.json(order)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async show(request: Request, response: Response, next: NextFunction) {
+    try {
+      const { table_session_id } = request.params
+
+      const order = await knex("orders")
+      .select(
+        knex.raw("COALESCE(SUM(orders.price * orders.quantity), 0) AS total"),
+        knex.raw("COALESCE(SUM(orders.quantity), 0) AS quantity")
+      )
+      .where({ table_session_id })
+      .first()
+
+      return response.json(order)
     } catch (error) {
       next(error)
     }
